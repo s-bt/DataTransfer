4abadca2badfe7a2dd7f3ff5481f81496dca3c64
s-bt <48731760+s-bt@users.noreply.github.com>
2021-08-31 16:46:12 +0200

    Update Invoke-Base64FileTransfer.ps1

--- Invoke-Base64FileTransfer.ps1
+++ Invoke-Base64FileTransfer.ps1
@@ -1,4 +1,75 @@
-Function Invoke-Base64FileCopy {
+Function ConvertTo-GZipString () {
+<#
+  .SYNOPSIS
+    Compresses a string with the GZip algorithm 
+    
+  .DESCRIPTION
+    Compresses a string with the GZip algorithm and returns the result in a Base64 string
+    
+  .PARAMETER String
+    Any plain text string to be compressed
+    
+  .EXAMPLE
+    dir | Out-String | ConvertTo-GZipString
+    
+  .LINK
+    ConvertFrom-GZipString
+#>
+
+  [CmdletBinding()]
+  Param(
+    [Parameter(Mandatory=$True,ValueFromPipeline=$True,ValueFromPipelinebyPropertyName=$True)]
+    $String
+  )
+
+  Process {
+    $String | ForEach-Object {
+      $ms = New-Object System.IO.MemoryStream
+      $cs = New-Object System.IO.Compression.GZipStream($ms, [System.IO.Compression.CompressionMode]::Compress)
+      $sw = New-Object System.IO.StreamWriter($cs)
+      $sw.Write($_)
+      $sw.Close()
+      [System.Convert]::ToBase64String($ms.ToArray())   
+    }
+  }
+}
+Function ConvertFrom-GZipString () {
+<#
+  .SYNOPSIS
+    Decompresses a Base64 GZipped string
+    
+  .DESCRIPTION
+    Decompresses a Base64 GZipped string
+    
+  .PARAMETER String
+    A Base64 encoded GZipped string
+    
+  .EXAMPLE
+    $compressedString | ConvertFrom-GZipString
+    
+  .LINK
+    ConvertTo-GZipString
+#>
+
+  [CmdletBinding()]
+  Param(
+    [Parameter(Mandatory=$True,ValueFromPipeline=$True,ValueFromPipelinebyPropertyName=$True)]
+    $String
+  )
+
+  Process {
+    $String | ForEach-Object {
+      $compressedBytes = [System.Convert]::FromBase64String($_)
+      $ms = New-Object System.IO.MemoryStream
+      $ms.write($compressedBytes, 0, $compressedBytes.Length)
+      $ms.Seek(0,0) | Out-Null
+      $cs = New-Object System.IO.Compression.GZipStream($ms, [System.IO.Compression.CompressionMode]::Decompress)
+      $sr = New-Object System.IO.StreamReader($cs)
+      $sr.ReadToEnd()
+    }
+  }
+}
+Function Invoke-Base64FileCopyPreparation {
     [CmdletBinding(DefaultParameterSetName = 'ToBase64')]
     param(
         [Parameter(Mandatory=$true,ParameterSetName='ToBase64')]
@@ -8,27 +79,23 @@ Function Invoke-Base64FileCopy {
     )
     
     if ($InputFile.Length -gt 0) {
-        $TempFileName = "$($env:temp)\temp$(Get-Random -Minimum 100 -Maximum 999).zip"
-        Compress-Archive -Path $InputFile -DestinationPath $TempFileName
-        $base64string = [Convert]::ToBase64String([IO.File]::ReadAllBytes($TempFileName))
-        Set-Clipboard -Value $base64string
-        Write-Host "[+] Base64 representation of '$($InputFile)' copied to your clipboard." -ForegroundColor Green
+        $base64string = [Convert]::ToBase64String([IO.File]::ReadAllBytes($InputFile))
+        Set-Clipboard -Value (ConvertTo-GZipString -String $base64string)
+        Write-Host "[+] Compressed base64 representation of '$($InputFile)' copied to your clipboard." -ForegroundColor Green
+
     } elseif ($OutputFile.Length -gt 0) {
         try {
-            $Content = [System.Convert]::FromBase64String((Get-Clipboard -ErrorAction Stop))
+            Set-Content -Path $OutputFile -Value ([System.Convert]::FromBase64String((ConvertFrom-GZipString -String (Get-Clipboard) -ErrorAction Stop))) -Encoding Byte
+            Write-Host "[+] File '$($OutputFile)' created from clipboard value" -ForegroundColor Green
         } catch {
-            Write-Host "[-] Error converting base64 string from clipboard value" -ForegroundColor Red
+            Write-Host "[-] Error uncompressing, base64 decoding and writing clipboard content to '$($OutputFile)'"
         }
-        $OutputZip = $OutputFile.Insert($OutputFile.Length,'.zip')
-        Set-Content -Path $OutputZip -Value $Content -Encoding Byte
-        Write-Host "[+] File '$($OutputZip)' created from clipboard value" -ForegroundColor Green
     } else {
             Write-Host "[-] You need to specify either an input or output file" -ForegroundColor Yellow
     }
 }
 
-# Base64 encode a file and copy the result to the clipboard (e.g. for transfering via RDP)
-# Invoke-Base64FileCopy -InputFile C:\tools\sharphound.exe -Verbose
 
-# Decode a base64 representation of file (exe, zip, ...) that's in your clipboard and write it to disk
-# Invoke-Base64FileCopy -OutputFile C:\tools\sharphound.exe -Verbose
+
+Invoke-Base64FileCopyPreparation -InputFile "C:\tools\kerbrute_windows_amd64.exe"
+Invoke-Base64FileCopyPreparation -OutputFile "C:\tools\kerbrute_windows_amd64_newer.exe"
